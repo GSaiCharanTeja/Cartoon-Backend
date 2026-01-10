92bcda318316e83c69bba294a3c3461e53f5bc57
G Sai Charan Teja <gavidisaicharanteja@gmail.com>
2026-01-11 01:08:11 +0530

    server.js is pushed

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// TEMP local DB (will change later)
+mongoose.connect("mongodb://127.0.0.1:27017/cartoonlikes")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Model
+const Like = mongoose.model("Like", {
+  name: String,
+  count: { type: Number, default: 0 }
+});
+
+// Like route
+app.post("/like", async (req, res) => {
+  const { name } = req.body;
+
+  await Like.findOneAndUpdate(
+    { name },
+    { $inc: { count: 1 } },
+    { upsert: true }
+  );
+
+  res.send("Liked");
+});
+
+// Admin route
+app.get("/admin", async (req, res) => {
+  const data = await Like.find();
+  res.json(data);
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
